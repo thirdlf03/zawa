94fbd5ffc85dbdefb84ffb0470528fde7bb8eb3f
thirdlf03 <thirdlf03@gmail.com>
2025-01-04 21:16:38 +0900

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,3 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js';
-import Stats from 'three/examples/jsm/libs/stats.module.js';
-import GUI from 'lil-gui';
-import * as CANNON from 'cannon-es';
-
 document.getElementById('add-ball')?.addEventListener('click', function() {
   const ballForm = document.getElementById('ball-form');
   if (ballForm) {
